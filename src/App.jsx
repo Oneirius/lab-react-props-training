7f32604855b9f3dd4070bb4580ed29fdada34d16
Kumar Daryanani <destral@msn.com>
2024-02-06 12:36:59 +0100

    Lab WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import "./components/IdCard"
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <IdCard
+        lastName="Doe" firstName="John" gender="Male" height={178} birth={new Date("1992-07-14")} picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
     </div>
   );
 }
---
+++ src/components/IdCard.jsx
@@ -0,0 +1,18 @@
+function IdCard() {
+    return (
+        <div>
+            <div>
+                <img src="#" alt="" />
+            </div>
+            <div>
+                <p>First Name: </p>
+                <p>Last Name: </p>
+                <p>Gender: </p>
+                <p>Height: </p>
+                <p>Birth: </p>
+            </div>
+        </div>
+    )
+}
+
+export default IdCard;
